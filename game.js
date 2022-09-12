b5bfb20efac49f7166199c5fb97659d06d08bbfe
jurijkreutz <jurij.kreutz97@gmail.com>
2022-09-12 11:59:58 +0200

    Create basic CSS for game

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
-initGame();
+document.addEventListener("keydown", (event) => {
+    initGame();
+  });
+
 
 function initGame() {
-
+    const bird = document.getElementById('flying-element');
+    bird.style.display = "block";
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
