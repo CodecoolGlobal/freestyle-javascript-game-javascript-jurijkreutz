42a2b1dc26cd1c70d406da0cf99f018f5c8b9535
jurijkreutz <jurij.kreutz97@gmail.com>
2022-09-14 15:31:58 +0200

    Add collision check

--- server.py
+++ server.py
@@ -9,6 +9,11 @@ def main():
     return render_template('index.html')
 
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
 @app.route("/register")
 def register():
     return render_template('register.html')
