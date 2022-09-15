18eeba7b25fe78f85b229109a60d219af8d84a3f
jurijkreutz <jurij.kreutz97@gmail.com>
2022-09-15 16:24:02 +0200

    Heroku changes

--- database_common.py
+++ database_common.py
@@ -2,29 +2,31 @@ import psycopg2.extras
 import psycopg2
 import os
 
+connection_string = os.environ.get('DATABASE_URL')
+connection = psycopg2.connect(connection_string)
 
-def get_connection_string():
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
+# def get_connection_string():
+#     user_name = os.environ.get('PSQL_USER_NAME')
+#     password = os.environ.get('PSQL_PASSWORD')
+#     host = os.environ.get('PSQL_HOST')
+#     database_name = 'flappy'
 
-    env_variables_defined = user_name and password and host and database_name
+#     env_variables_defined = user_name and password and host and database_name
 
-    if env_variables_defined:
-        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-            user_name=user_name,
-            password=password,
-            host=host,
-            database_name=database_name
-        )
-    else:
-        raise KeyError('Some necessary environment variable(s) are not defined')
+#     if env_variables_defined:
+#         return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+#             user_name=user_name,
+#             password=password,
+#             host=host,
+#             database_name=database_name
+#         )
+#     else:
+#         raise KeyError('Some necessary environment variable(s) are not defined')
 
 
 def open_database():
     try:
-        connection_string = get_connection_string()
+        # connection_string = get_connection_string()
         connection = psycopg2.connect(connection_string)
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
