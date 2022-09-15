ed0a26fe5057b04a40c533033a000f2d9b4f0ccb
jurijkreutz <jurij.kreutz97@gmail.com>
2022-09-15 09:08:25 +0200

    Add Server functions

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ def login():
     return render_template('login.html')
 
 
-@app.route("/register", methods=['POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
 
     return render_template('register.html')
